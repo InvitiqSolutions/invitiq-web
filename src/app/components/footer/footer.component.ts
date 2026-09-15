043eb3442887b440ee04c807bfec903eed0dbec0
Shabari Prasannaa <invitiq@gmail.com>
2026-09-15 23:35:19 +0530

    Updated footer contact

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -13,15 +13,52 @@ import { RouterModule } from '@angular/router';
 export class FooterComponent {
   constructor(protected service : CommonService) { }
 
-  enquiry = {
-  name: '',
-  email: '',
-  message: ''
-};
+  contact = {
+    name: '',
+    phone: '',
+    company: '',
+    service: '',
+    query: ''
+  };
 
-submitEnquiry() {
+submitContact() {
 
-  console.log(this.enquiry);
+  const formData = new URLSearchParams();
+
+    formData.set('form-name', 'enquiry');
+    formData.set('name', this.contact.name);
+    formData.set('phone', this.contact.phone);
+    formData.set('company', this.contact.company);
+    formData.set('service', this.contact.service);
+    formData.set('query', this.contact.query);
+
+    fetch('/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: formData.toString()
+    })
+    .then(() => {
+
+      alert('Thank you! Your enquiry has been sent successfully.');
+
+      this.contact = {
+        name: '',
+        phone: '',
+        company: '',
+        service: '',
+        query: ''
+      };
+
+    })
+    .catch(error => {
+
+      console.error(error);
+
+      alert('Unable to send your enquiry. Please try again.');
+
+    });
 
 }
 
